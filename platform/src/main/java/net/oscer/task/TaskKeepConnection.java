d2c96b8a83b8ef356a3a85d23f8ff358f1475ec6
柯真 <kz52891177>
2019-11-05 12:33:04 +0800

    【优化】以定时器方式去查询数据，解决mysql 8小时内无连接的问题

--- platform/src/main/java/net/oscer/task/TaskKeepConnection.java
+++ platform/src/main/java/net/oscer/task/TaskKeepConnection.java
@@ -16,11 +16,11 @@ import org.springframework.stereotype.Component;
 public class TaskKeepConnection {
 
     /**
-     * 每隔1小时查询
+     * 每隔10分钟查询
      *
      * @throws Exception
      */
-    @Scheduled(cron = "0 0 0/1 * * ?")
+    @Scheduled(cron = "0 */10 * * * ?")
     public void timer() throws Exception {
         DbQuery.get("mysql").transaction(new TransactionService() {
             @Override
