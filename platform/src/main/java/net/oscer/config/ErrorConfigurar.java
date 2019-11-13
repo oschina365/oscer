300db4f3dbd94522500975e3a69c87ff8996b07b
柯真 <kz52891177>
2019-11-13 10:31:34 +0800

    【优化】全局异常处理

--- platform/src/main/java/net/oscer/config/ErrorConfigurar.java
+++ platform/src/main/java/net/oscer/config/ErrorConfigurar.java
@@ -3,6 +3,7 @@ package net.oscer.config;
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.ErrorPageRegistrar;
 import org.springframework.boot.web.server.ErrorPageRegistry;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
@@ -12,14 +13,15 @@ import org.springframework.stereotype.Component;
  * @author kz
  * @date 2019年3月14日16:26:55
  */
-@Component
+@Configuration
 public class ErrorConfigurar implements ErrorPageRegistrar {
 
     @Override
     public void registerErrorPages(ErrorPageRegistry registry) {
-        ErrorPage[] errorPages = new ErrorPage[2];
-        errorPages[0] = new ErrorPage(HttpStatus.NOT_FOUND, "/error404");
-        errorPages[1] = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error500");
+        ErrorPage[] errorPages = new ErrorPage[3];
+        errorPages[0] = new ErrorPage(HttpStatus.NOT_FOUND, "/error/404");
+        errorPages[1] = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error/500");
+        errorPages[2] = new ErrorPage(HttpStatus.BAD_REQUEST, "/error/403");
         registry.addErrorPages(errorPages);
     }
 }
